a9c357e592414846b43ef0a8b566e3e0ac433da8
Taras Murzenkov <taras.murzenkov@freelancers.outfittery.de>
2022-04-21 18:25:18 +0200

    initial commit

---
+++ src/main/java/com/celonis/challenge/services/FileService.java
@@ -0,0 +1,52 @@
+package com.celonis.challenge.services;
+
+import com.celonis.challenge.exceptions.InternalException;
+import com.celonis.challenge.model.ProjectGenerationTask;
+import com.celonis.challenge.model.ProjectGenerationTaskRepository;
+import org.apache.commons.io.IOUtils;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Component;
+
+import java.io.*;
+import java.net.URL;
+
+@Component
+public class FileService {
+
+    private final TaskService taskService;
+
+    private final ProjectGenerationTaskRepository projectGenerationTaskRepository;
+
+    public FileService(TaskService taskService,
+                       ProjectGenerationTaskRepository projectGenerationTaskRepository) {
+        this.taskService = taskService;
+        this.projectGenerationTaskRepository = projectGenerationTaskRepository;
+    }
+
+    public FileSystemResource getTaskResult(String taskId) {
+        ProjectGenerationTask projectGenerationTask = taskService.getTask(taskId);
+        File inputFile = new File(projectGenerationTask.getStorageLocation());
+
+        if (!inputFile.exists()) {
+            throw new InternalException("File not generated yet");
+        }
+
+        return new FileSystemResource(inputFile);
+    }
+
+    public void storeResult(String taskId, URL url) throws IOException {
+        ProjectGenerationTask projectGenerationTask = taskService.getTask(taskId);
+        File outputFile = File.createTempFile(taskId, ".zip");
+        outputFile.deleteOnExit();
+        projectGenerationTask.setStorageLocation(outputFile.getAbsolutePath());
+        projectGenerationTaskRepository.save(projectGenerationTask);
+        try (InputStream is = url.openStream();
+             OutputStream os = new FileOutputStream(outputFile)) {
+            IOUtils.copy(is, os);
+        }
+    }
+}
